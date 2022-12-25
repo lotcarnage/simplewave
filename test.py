07738d263bfce7699224887f1a7853b9690f5fe5
lotcarnage <lotcarnage@gmail.com>
2022-12-26 02:12:56 +0900

    [change] numpyに依存しない独自実装に変更しました。

--- test.py
+++ test.py
@@ -1,37 +1,54 @@
 #! /usr/local/bin/python3
-from simplewave import fetch
+import unittest
+import simplewave
 import os.path
 
 
-def _check_fetch(test_data_file_path: str, expected_Fs: int, expected_nch: int, expected_byte_depth: int, expected_num_samples: int) -> bool:
-    actual_Fs, actual_nch, actual_byte_depth, actual_num_samples = fetch(test_data_file_path)
-    is_succeeded = True
-    if actual_Fs != expected_Fs:
-        print(f'[FAILED] Fs: expected {expected_Fs}, actual {actual_Fs}')
-        is_succeeded = False
-    if actual_nch != expected_nch:
-        print(f'[FAILED] nch: expected {expected_nch}, actual {actual_nch}')
-        is_succeeded = False
-    if actual_byte_depth != expected_byte_depth:
-        print(f'[FAILED] byte_depth: expected {expected_byte_depth}, actual {actual_byte_depth}')
-        is_succeeded = False
-    if actual_num_samples != expected_num_samples:
-        print(f'[FAILED] num_samples: expected {expected_num_samples}, actual {actual_num_samples}')
-        is_succeeded = False
-    return is_succeeded
+def _compare(left_hand_file_path: str, right_hand_file_path: str) -> bool:
+    with open(left_hand_file_path, 'rb') as lhf, open(right_hand_file_path, 'rb') as rhf:
+        lf_bytes = lhf.read()
+        rf_bytes = rhf.read()
+    if len(lf_bytes) != len(rf_bytes):
+        return False
+    for lhv, rhv in zip(lf_bytes, rf_bytes):
+        if lhv != rhv:
+            return False
+    return True
 
 
-def _main():
-    TEST_ROOT = os.path.dirname(__file__)
-    is_succeeded = True
-    if not _check_fetch(f'{TEST_ROOT}/testdata/test_monoral_48000_16bit.wav', 48000, 1, 2, 240000):
-        print('[FAILED] Fetch Test Case 1')
-        is_succeeded = False
-    if not _check_fetch(f'{TEST_ROOT}/testdata/test_stereo_44100_16bit.wav', 44100, 2, 2, 328704):
-        print('[FAILED] Fetch Test Case 1')
-        is_succeeded = False
-    return 0 if is_succeeded else 1
+TEST_ROOT = os.path.dirname(__file__)
+
+
+class TestSimpleWave(unittest.TestCase):
+    def test_fetch_monoral_wave(self):
+        src = f'{TEST_ROOT}/testdata/test_monoral_48000_16bit.wav'
+        actual_Fs, actual_pcm_type, actual_nch, actual_num_samples = simplewave.fetch(src)
+        self.assertEqual(actual_Fs, 48000)
+        self.assertEqual(actual_pcm_type, simplewave.PcmFormat.SINT16)
+        self.assertEqual(actual_nch, 1)
+        self.assertEqual(actual_num_samples, 240000)
+
+    def test_fetch_stereo_wave(self):
+        src = f'{TEST_ROOT}/testdata/test_stereo_44100_16bit.wav'
+        actual_Fs, actual_pcm_type, actual_nch, actual_num_samples = simplewave.fetch(src)
+        self.assertEqual(actual_Fs, 44100)
+        self.assertEqual(actual_pcm_type, simplewave.PcmFormat.SINT16)
+        self.assertEqual(actual_nch, 2)
+        self.assertEqual(actual_num_samples, 328704)
+
+    def test_load_and_save_monoral(self):
+        src = f'{TEST_ROOT}/testdata/test_monoral_48000_16bit.wav'
+        dst = f'{TEST_ROOT}/testdata/test_monoral_48000_16bit.out.wav'
+        self.assertTrue(simplewave.save(dst, *simplewave.load(src)[:3]))
+        self.assertTrue(_compare(src, dst))
+
+    def test_load_and_save_stereo(self):
+        TEST_ROOT = os.path.dirname(__file__)
+        src = f'{TEST_ROOT}/testdata/test_stereo_44100_16bit.wav'
+        dst = f'{TEST_ROOT}/testdata/test_stereo_44100_16bit.out.wav'
+        self.assertTrue(simplewave.save(dst, *simplewave.load(src)[:3]))
+        self.assertTrue(_compare(src, dst))
 
 
 if __name__ == '__main__':
-    exit(_main())
+    unittest.main()
